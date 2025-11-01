de009b0d4fb9738a9c99f5cc345e91b8decf9feb
akashravindra029 <akashravindra029@gmail.com>
2025-11-01 23:42:36 +0530

    Delete Desktop/frntend/frntend_pharmacy-main directory

--- Desktop/frntend/frntend_pharmacy-main/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders dashboard button', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Dashboard/i);
-  expect(linkElement).toBeInTheDocument();
-});
--- Desktop/frntend/frntend_pharmacy-main/src/setupTests.js
+++
@@ -1,7 +0,0 @@
-import '@testing-library/jest-dom';
-
-// Add global TextEncoder and TextDecoder for tests
-import { TextEncoder, TextDecoder } from 'util';
-
-global.TextEncoder = TextEncoder;
-global.TextDecoder = TextDecoder;
